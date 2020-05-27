a88f37f7572facfbcbd4bb50539e2ffa9257eca5
Lai Jiang <jianglai.china@aliyun.com>
2020-05-27 13:53:01 +0800

    u

--- test_our2.py
+++ test_our2.py
@@ -36,6 +36,19 @@ if __name__ == "__main__":
     sess_config = tf.ConfigProto()
     sess_config.gpu_options.allow_growth = True
     with tf.Session(config=sess_config) as sess:
+      imin = tf.placeholder(tf.float32, (1, None, None, 6))
+      output = model.build_server_graph(FLAGS, imin)
+      output = (output + 1.) * 127.5
+      output = tf.reverse(output, [-1])
+      output = tf.saturate_cast(output, tf.uint8)
+      # load pretrained model
+      vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+      assign_ops = []
+      for var in vars_list:
+        vname = var.name
+        from_name = vname
+        var_value = tf.contrib.framework.load_variable(checkpoint_dir, from_name)
+        assign_ops.append(tf.assign(var, var_value))
         for x in images:
             curdir = os.path.join(inputdir, x)
             if os.path.isdir(curdir):
@@ -52,22 +65,10 @@ if __name__ == "__main__":
                 image = np.expand_dims(image, 0)
                 mask = np.expand_dims(mask, 0)
                 input_image = np.concatenate([image, mask], axis=2)
-
                 input_image = tf.constant(input_image, dtype=tf.float32)
-                output = model.build_server_graph(FLAGS, input_image, reuse=True)
-                output = (output + 1.) * 127.5
-                output = tf.reverse(output, [-1])
-                output = tf.saturate_cast(output, tf.uint8)
-                # load pretrained model
-                vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-                assign_ops = []
-                for var in vars_list:
-                    vname = var.name
-                    from_name = vname
-                    var_value = tf.contrib.framework.load_variable(checkpoint_dir, from_name)
-                    assign_ops.append(tf.assign(var, var_value))
+
                 sess.run(assign_ops)
                 print('Model loaded.')
-                result = sess.run(output)
+                result = sess.run(output, feed_dict={imin: input_image})
 
                 cv2.imwrite(os.path.join(outdir, x + '.png'), result[0][:, :, ::-1])
