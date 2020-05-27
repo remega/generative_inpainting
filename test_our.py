59188a222ad7849fe90a2f2a685be052575dda88
Lai Jiang <jianglai.china@aliyun.com>
2020-05-27 15:16:08 +0800

    u

--- test_our.py
+++ test_our.py
@@ -19,8 +19,8 @@ from inpaint_model import InpaintCAModel
 # parser.add_argument('--checkpoint_dir', default='', type=str,
 #                     help='The directory of tensorflow checkpoint.')
 
-inputdir = '../ValRemoval/SGITS'
-outdir = './results/SGITS'
+inputdir = '../ValRemoval/SGITR'
+outdir = './results/SGITR'
 checkpoint_dir = './checkpoints/snap-0'
 
 if not os.path.exists(outdir):
@@ -33,41 +33,50 @@ if __name__ == "__main__":
     model = InpaintCAModel()
     images = os.listdir(inputdir)
     images.sort()
-
-    for x in images:
+    sess_config = tf.ConfigProto()
+    sess_config.gpu_options.allow_growth = True
+    with tf.Session(config=sess_config) as sess:
+      imin = tf.placeholder(tf.float32, (1, 320, 960, 3))
+      output = model.build_server_graph(FLAGS, imin)
+      output = (output + 1.) * 127.5
+      output = tf.reverse(output, [-1])
+      output = tf.saturate_cast(output, tf.uint8)
+      # load pretrained model
+      vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+      assign_ops = []
+      for var in vars_list:
+        vname = var.name
+        from_name = vname
+        var_value = tf.contrib.framework.load_variable(checkpoint_dir, from_name)
+        assign_ops.append(tf.assign(var, var_value))
+      sess.run(assign_ops)
+      for x in images:
         curdir = os.path.join(inputdir, x)
         if os.path.isdir(curdir):
-            image = cv2.imread(os.path.join(curdir, 'Ori.png'))
-            mask = cv2.imread(os.path.join(curdir, 'mask.png'))
-            assert image.shape == mask.shape
+            gfile = glob.glob(os.path.join(curdir, 'GT*.jpg'))
+            gfile.sort()
+            for fulldir in gfile:
 
-            h, w, _ = image.shape
-            grid = 8
-            image = image[:h//grid*grid, :w//grid*grid, :]
-            mask = mask[:h//grid*grid, :w//grid*grid, :]
-            print('Shape of image: {}'.format(image.shape))
+                fullname = fulldir.split('/')[-1]
+                tempindex = fullname[-5:-4]
 
-            image = np.expand_dims(image, 0)
-            mask = np.expand_dims(mask, 0)
-            input_image = np.concatenate([image, mask], axis=2)
-            sess_config = tf.ConfigProto()
-            sess_config.gpu_options.allow_growth = True
-            with tf.Session(config=sess_config) as sess:
-                input_image = tf.constant(input_image, dtype=tf.float32)
-                output = model.build_server_graph(FLAGS, input_image, reuse=True)
-                output = (output + 1.) * 127.5
-                output = tf.reverse(output, [-1])
-                output = tf.saturate_cast(output, tf.uint8)
-                # load pretrained model
-                vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-                assign_ops = []
-                for var in vars_list:
-                    vname = var.name
-                    from_name = vname
-                    var_value = tf.contrib.framework.load_variable(checkpoint_dir, from_name)
-                    assign_ops.append(tf.assign(var, var_value))
-                sess.run(assign_ops)
+                image = cv2.imread(os.path.join(curdir, 'Ori.jpg'))
+                mask = cv2.imread(os.path.join(curdir, 'mask_' + tempindex + '.png'))
+                assert image.shape == mask.shape
+
+                h, w, _ = image.shape
+                grid = 8
+                image = image[:h//grid*grid, :w//grid*grid, :]
+                mask = mask[:h//grid*grid, :w//grid*grid, :]
+                print('Shape of image: {}'.format(image.shape))
+
+                image = np.expand_dims(image, 0)
+                mask = np.expand_dims(mask, 0)
+                input_image = np.concatenate([image, mask], axis=2)
+                input_image = input_image.astype(np.float32)
+                # input_image = tf.constant(input_image, dtype=tf.float32)
+
                 print('Model loaded.')
-                result = sess.run(output)
+                result = sess.run(output, feed_dict={imin: input_image})
 
-                cv2.imwrite(os.path.join(outdir, x + '.png'), result[0][:, :, ::-1])
+                cv2.imwrite(os.path.join(outdir, x + '_' + tempindex + '.png'), result[0][:, :, ::-1])
